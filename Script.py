6433dd0664085c7a5f718fce8ba119b344162c82
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-09 23:53:12 +0600

    take input csv file from local pc

--- Script.py
+++ Script.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 # Load the train and test datasets to create two DataFrames
 train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-train = pd.read_csv(train_url)
+train = pd.read_csv("train.csv")
 
 test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
-test = pd.read_csv(test_url)
+test = pd.read_csv("test.csv")
 
 
 #Print the `head` of the train and test dataframes
