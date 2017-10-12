4b6f7bccf1792f8404607fb36ea87b7eeb424fdf
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-12 22:11:56 +0600

    print another way

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -10,23 +10,21 @@ from sklearn import tree
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
  
-
-
  
-# # Convert the male and female groups to integer form
-# train["Sex"][train["Sex"] == "male"] = 0
-# train["Sex"][train["Sex"] == "female"] = 1
+# Convert the male and female groups to integer form
+train["Sex"][train["Sex"] == "male"] = 0
+train["Sex"][train["Sex"] == "female"] = 1
 
-# # Impute the Embarked variable
-# train["Embarked"] = train["Embarked"].fillna("S")
+# Impute the Embarked variable
+train["Embarked"] = train["Embarked"].fillna("S")
 
 
-# # Convert the Embarked classes to integer form
-# train["Embarked"] [ train["Embarked"] =='S' ] = 0;  
-# train["Embarked"] [ train["Embarked"] =='C' ] = 1;  
-# train["Embarked"] [ train["Embarked"] =='Q' ] = 2;  
+# Convert the Embarked classes to integer form
+train["Embarked"] [ train["Embarked"] =='S' ] = 0;  
+train["Embarked"] [ train["Embarked"] =='C' ] = 1;  
+train["Embarked"] [ train["Embarked"] =='Q' ] = 2;  
 
 
 #Print the Sex and Embarked columns
-print (train["Sex"]["Embarked"] )
-#print (train.loc[ :, ["Sex","Embarked"] ]  )
+print (train [["Sex","Embarked"] ] ).head()
+print (train.loc[ :, ["Sex","Embarked"] ]  ) . head()
