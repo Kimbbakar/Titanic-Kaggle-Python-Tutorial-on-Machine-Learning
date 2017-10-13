d6829ad0dd7562592f48d85e5a0a2b643c75c515
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-13 23:36:57 +0600

    solve the issue SettingWithCopyWarning

--- Creating Decision Tree.py
+++ Creating Decision Tree.py
@@ -10,20 +10,22 @@ test = pd.read_csv("test.csv")
  
  
 # Convert the male and female groups to integer form
-train["Sex"][train["Sex"] == "male"] = 0
-train["Sex"][train["Sex"] == "female"] = 1
+train.loc[train["Sex"] == "male",'Sex' ] = 0
+train.loc[train["Sex"] == "female",'Sex' ] = 1
+
 
 # Impute the Embarked variable
 train["Embarked"] = train["Embarked"].fillna("S")
 
 
 # Convert the Embarked classes to integer form
-train["Embarked"] [ train["Embarked"] =='S' ] = 0;  
-train["Embarked"] [ train["Embarked"] =='C' ] = 1;  
-train["Embarked"] [ train["Embarked"] =='Q' ] = 2;  
+train.loc[ train["Embarked"]=='S', "Embarked" ] = 0
+train.loc[ train["Embarked"]=='C', "Embarked" ] = 1
+train.loc[ train["Embarked"]=='Q', "Embarked" ] = 2
+  
 
 # Print the train data to see the available features
-#print(train.head())
+print(train.head())
 
 # Create the target and features numpy arrays: target, features_one
 target = np.array(train["Survived" ].values )
@@ -36,7 +38,7 @@ my_tree_one = my_tree_one.fit(features_one,target)
 
 
 
-# Look at the importance and score of the included features
-print(my_tree_one.feature_importances_)
-print(my_tree_one.score(features_one, target))
+# # Look at the importance and score of the included features
+# print(my_tree_one.feature_importances_)
+# print(my_tree_one.score(features_one, target))
 
